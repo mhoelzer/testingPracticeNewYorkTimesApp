4e44b19e26edddff10b4aac956ced4726f433a0f
Michelle Hoelzer <mhoelzer@pio.carrollu.edu>
2018-03-12 16:48:45 -0400

    finished the basics

--- nyt.js
+++ nyt.js
@@ -1,6 +1,6 @@
-const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'; //1
-const key = '820acff1756345ed8c02711ba87f782c'; //2
-let url; //3
+const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'; 
+const key = '820acff1756345ed8c02711ba87f782c'; 
+let url; 
 
 //SEARCH FORM
 const searchTerm = document.querySelector('.search');
@@ -14,26 +14,121 @@ const nextBtn = document.querySelector('.next');
 const previousBtn = document.querySelector('.prev');
 const nav = document.querySelector('nav');
 
+let pageNumber = 0;
+console.log('PageNumber:', pageNumber);
+let displayNav = false;
+
 //RESULTS SECTION
 const section = document.querySelector('section');
 
-nav.style.display = 'none';
+// nav.style.display = 'none';
 
-let pageNumber = 0;
-let displayNav = false;
+// let pageNumber = 0;
+// console.log('PageNumber:', pageNumber);
+// let displayNav = false;
 
-//1
+searchForm.addEventListener('submit', fetchResults); 
+nextBtn.addEventListener('click', nextPage); 
+previousBtn.addEventListener('click', previousPage); 
+
+
 function fetchResults(e) {
-  console.log(e); //2
-  // Assemble the full URL
-  url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value; //3
-  console.log(url); //4
+	e.preventDefault(); 
+	// Assemble the full URL
+	url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value;
+	console.log("URL:", url);
+	if(startDate.value !== '') {
+		console.log(startDate.value)
+		url += '&begin_date=' + startDate.value;
+	};
+	if(endDate.value !== '') {
+		url += '&end_date=' + endDate.value;
+	};
+	  
+	fetch(url)
+		.then(function(result) {
+    	// console.log(result)
+    	return result.json(); 
+	}).then(function(json) {
+		// console.log(json); 
+		displayResults(json); 
+	});
 }
-
 function nextPage(){
-  console.log("Next button clicked");
-} //5
-
+	console.log("Next button clicked");
+}
 function previousPage(){
-  console.log("Next button clicked");
-} //5
+	console.log("Next button clicked");
+} 
+
+function displayResults(json) {
+	while (section.firstChild) {
+      section.removeChild(section.firstChild); 
+	}
+	console.log("DisplayResults", json); 
+	console.log(json.response.docs);
+	let articles = json.response.docs;
+	console.log(articles);
+	if(articles.length === 0) {
+    	console.log("No results");
+	} else {
+		for(let i = 0; i < articles.length; i++) {
+			let article = document.createElement('article'); 
+			let heading = document.createElement('h2'); 
+			let link = document.createElement('a'); 
+			let img = document.createElement('img');
+			let para = document.createElement('p');
+			let clearfix = document.createElement('div');
+			let current = articles[i]; 
+			console.log("Current:", current); 
+			link.href = current.web_url; 
+			link.textContent = current.headline.main; 
+			para.textContent = 'Keywords: ';
+
+			for(let j = 0; j < current.keywords.length; j++) {
+		        let span = document.createElement('span');   
+				span.textContent += current.keywords[j].value + ' ';   
+				para.appendChild(span);
+			}
+
+			if(current.multimedia.length > 0) {
+				img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
+				img.alt = current.headline.main;
+			}
+
+			clearfix.setAttribute('class','clearfix');
+
+			article.appendChild(heading);
+			heading.appendChild(link);
+			article.appendChild(img);
+			article.appendChild(para);
+			article.appendChild(clearfix);
+			section.appendChild(article);
+		}
+	}
+	if(articles.length === 10) {
+		nav.style.display = 'block';
+	} else {
+		nav.style.display = 'none';
+	}
+};
+
+function nextPage(e) {
+	pageNumber++; 
+	fetchResults(e);  
+	console.log("Page number:", pageNumber); 
+};
+function previousPage(e) {
+  if(pageNumber > 0) { 
+    pageNumber--; 
+  } else {
+    return; 
+  }
+  fetchResults(e); 
+  console.log("Page:", pageNumber); 
+};
+
+
+
+
+
